fff59f770a25161ed0de6a7c4e59df3988e34e99
zimelemonta <ziemelemonta@gmail.com>
2024-11-08 11:59:24 +0200

    Add files via upload

---
+++ temp monta.py
@@ -0,0 +1,42 @@
+x=int(5)
+ 
+int(x)
+float skaitlis = 3,14
+
+
+skaitlis=float(input)("5")
+ifx>5
+print("")
+elif<0
+print("")
+else
+print("")
+
+if atdzīme:9
+ print:(A)
+if atdzīme>:7
+ print:(B)
+ if atdzīme>:5
+ print:(c)
+ if atdzime>3
+ print:(D)
+ if atdzime>1
+ print:(E)
+ 
+ 
+ 
+x=int(input("x:"))
+y=int(input)("y:")
+result1=(4**(y*3))+15y
+print(result1)
+
+x=int(input)("x:"))
+y=int(input)("y:"))
+result2=5y**x-144x+2/(x+y)**2
+print(result2)
+
+x=int(input)("x:"))
+y=int(input)("y:"))
+result3=(2+x)-(2*x*y))/y/(x+y)
+print(result3)
+
